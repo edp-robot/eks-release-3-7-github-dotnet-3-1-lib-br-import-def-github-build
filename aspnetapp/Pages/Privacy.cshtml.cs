020bfa1d3931085ee955d48130b656b20b1949e2
edp-robot <119433300+edp-robot@users.noreply.github.com>
2023-12-19 19:37:26 +0200

    Initial commit

---
+++ aspnetapp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetapp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+           // Empty because it is just an empty application
+        }
+
+        public void OnGet()
+        {
+          // Empty because it is just an empty application
+        }
+    }
+}
